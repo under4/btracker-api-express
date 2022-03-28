5bfa087d63b4bb85273c01b06ca51fd0c4c43ce7
under4 <44830099+under4@users.noreply.github.com>
2022-03-28 21:25:26 +0300

    database test

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
+//require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 let port = process.env["port"] || 3000;
-/*
+
 const dbKey = process.env["db"];
 const db = mongoose.connect(dbKey, () => {
     console.log("connected to database");
 });
-*/
+
 app.get("/", (req, res) => {
     console.log(req);
     res.json({ greeting: "hello world" });
