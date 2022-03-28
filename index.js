caacccab3e2cc8940d903377c941df9fb6b84987
under4 <44830099+under4@users.noreply.github.com>
2022-03-28 21:22:55 +0300

    database error

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 let port = process.env["port"] || 3000;
-
+/*
 const dbKey = process.env["db"];
 const db = mongoose.connect(dbKey, () => {
     console.log("connected to database");
 });
-
+*/
 app.get("/", (req, res) => {
     console.log(req);
     res.json({ greeting: "hello world" });
