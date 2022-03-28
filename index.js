9e956108207b1abfb31d902da070dc42d747de86
under4 <44830099+under4@users.noreply.github.com>
2022-03-28 19:55:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    console.log(req);
+    res.json({ greeting: "hello world" });
+});
+
+app.listen(port, () => {
+    console.log(`App is listening at ${port}`);
+});
