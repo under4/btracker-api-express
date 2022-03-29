dfc59b80fc08d3e26dbc9011368bba86d7cbc1db
under4 <uonder50@gmail.com>
2022-03-30 00:36:34 +0300

    register

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 const app = express();
 console.log(process.env)
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
+const User = require("./Schema/User")
 
 const dbKey = process.env["db"];
 const db = mongoose.connect(dbKey, () => {
@@ -16,6 +19,11 @@ app.get("/", (req, res) => {
     //console.log(req);
     res.json({ greeting: "hello world" });
 });
+
+//new user
+app.post("/register",urlencodedParser, (req, res)=>{
+    console.log(req)
+})
 
 app.listen(port, () => {
     console.log(`App is listening at ${port}`);
