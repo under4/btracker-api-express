28f2b773629ca33e69a880ac8daede707b8ec6a8
under4 <44830099+under4@users.noreply.github.com>
2022-03-30 21:44:05 +0300

    response data

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser")
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const app = express();
-console.log(process.env)
 const port = process.env.PORT || 5000;
 
-const User = require("./Schema/User")
+const cors = require("cors");
+app.use(cors());
+
+const User = require("./Schema/User");
 
 const dbKey = process.env["db"];
 const db = mongoose.connect(dbKey, () => {
     console.log("connected to database");
 });
 
-
 app.get("/", (req, res) => {
-    //console.log(req);
+    console.log(req);
     res.json({ greeting: "hello world" });
 });
 
 //new user
-app.post("/register",urlencodedParser, (req, res)=>{
-    console.log(req)
-})
+app.post("/register", urlencodedParser, (req, res) => {
+    console.log(req);
+});
 
 app.listen(port, () => {
     console.log(`App is listening at ${port}`);
