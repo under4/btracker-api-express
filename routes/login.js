e0d3f73ac093b1c34f32de5cb5b97955ac97d3e9
under4 <44830099+under4@users.noreply.github.com>
2022-07-15 23:23:01 +0300

    test

--- routes/login.js
+++ routes/login.js
@@ -37,7 +37,7 @@ loginRouter.post("/register", async (req, res) => {
 
 loginRouter.post(
     "/login",
-    passport.authenticate("local", {
+    passport.authenticate("jwt", {
         successRedirect: `${process.env.APP_URL}/console`,
         failureRedirect: `${process.env.APP_URL}/login`,
         failureFlash: true,
