232bb67e4cb01b46ca48aaebb2d17a9d36e124d3
under4 <44830099+under4@users.noreply.github.com>
2022-07-16 21:35:30 +0300

    debug

--- routes/login.js
+++ routes/login.js
@@ -60,6 +60,7 @@ loginRouter.delete("/logout", (req, res) => {
 });
 
 loginRouter.get("/auth", (req, res) => {
+    console.log(req);
     if (req.isAuthenticated()) {
         return res.json({ err: 0 });
     } else {
