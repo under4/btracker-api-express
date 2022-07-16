e5c7ce1e2015e01633e652ca8002f7c133dee9a5
under4 <44830099+under4@users.noreply.github.com>
2022-07-16 19:25:33 +0300

    session problems

--- server.js
+++ server.js
@@ -43,6 +43,17 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    );
+    next();
+});
+
 const Comment = require("./Schema/Comment");
 const User = require("./Schema/User.js");
 const Bug = require("./Schema/Bug");
