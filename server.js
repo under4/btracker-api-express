9e3bff5adebef1b67c6c1cf5ca78da721eb988dd
under4 <44830099+under4@users.noreply.github.com>
2022-07-16 19:28:16 +0300

    bug

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(
         credentials: true,
     })
 );
+app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: false }));
 app.use(flash());
