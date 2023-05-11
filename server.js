76d7711a48c94058e6a82d70ced14cbe953fcdd4
under4 <uonder50@gmail.com>
2023-05-11 15:09:58 +0300

    try

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(
         secret: process.env.SESSION_SECRET,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+            secure: true,
         },
         saveUninitialized: false,
         resave: false,
