12c44c51fc9f894c4a5780eb8b87b820c51f8672
uygaronder <uonder50@gmail.com>
2023-07-10 01:30:02 +0300

    server test

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(
         secret: process.env.SESSION_SECRET,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-            secure: true,
         },
         saveUninitialized: true,
         resave: false,
