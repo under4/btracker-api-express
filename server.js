1a7dba4dcf47f189fde79e81f4dd10162fd3607d
Uygar <uonder50@gmail.com>
2023-07-20 14:35:07 +0300

    stggg

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(flash());
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
+        cookieName: 'session',
         cookie: {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
